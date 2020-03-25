bd508477289643a9a5712a5035698a3a63ddd2a2
Brandon Gardner <brandon.gardner2@outlook.com>
2020-03-25 09:21:49 -0500

    adds a delete icon which can be used to remove active metrics

--- src/Features/Metrics/LastUpdate/LastUpdateCard.tsx
+++ src/Features/Metrics/LastUpdate/LastUpdateCard.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { ListItem, ListItemText, makeStyles } from '@material-ui/core';
+import { ListItem, ListItemText, makeStyles, ListItemIcon } from '@material-ui/core';
 import { MetricData } from '../reducer';
 // I made a definitions file just to have prettier display names.
 // Typically I would expect some kind of display name from the API.
 // but this can be used in absence assuming all possible values are known ahead of time.
 import definitions from '../utils/definitions';
+import DeleteIcon from '@material-ui/icons/HighlightOff';
 
 const useStyles = makeStyles({
   listItem: {
     boxShadow: '0px 0px 1px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 1px 0px rgba(0,0,0,0.12)',
     borderRadius: '5px',
     margin: '4px 0',
+  },
+  listIcon: {
+    minWidth: 'auto',
+    color: 'red',
+
+    '&:hover': {
+      cursor: 'pointer',
+    },
   },
 });
 
@@ -28,6 +37,9 @@ const LastUpdateCard = ({ data }: OwnProps) => {
         secondary={`Latest value: ${data.value} ${data.unit}`}
         primaryTypographyProps={{ variant: 'h6' }}
       />
+      <ListItemIcon className={classes.listIcon}>
+        <DeleteIcon />
+      </ListItemIcon>
     </ListItem>
   );
 };
