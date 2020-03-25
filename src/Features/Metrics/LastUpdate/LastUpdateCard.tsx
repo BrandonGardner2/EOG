a336846d8e80d5db8995a9ce42d693e848212030
Brandon Gardner <brandon.gardner2@outlook.com>
2020-03-25 01:43:11 -0500

    updates a little styling on latest update items to look more in line with the rest of the box shadows being used by material ui

--- src/Features/Metrics/LastUpdate/LastUpdateCard.tsx
+++ src/Features/Metrics/LastUpdate/LastUpdateCard.tsx
@@ -4,9 +4,9 @@ import { MetricData } from '../reducer';
 
 const useStyles = makeStyles({
   listItem: {
-    border: '1px solid rgba(0, 0, 0, 0.15)',
+    boxShadow: '0px 0px 1px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 1px 0px rgba(0,0,0,0.12)',
     borderRadius: '5px',
-    margin: '3px 0',
+    margin: '4px 0',
   },
 });
 
