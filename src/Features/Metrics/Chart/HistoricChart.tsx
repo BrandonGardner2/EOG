e4308bf8848412600309287db1a0b7d5df013d23
Brandon Gardner <brandon.gardner2@outlook.com>
2020-03-25 18:30:39 -0500

    moves chart header to its own component since it will be used in live chart as well

--- src/Features/Metrics/Chart/HistoricChart.tsx
+++ src/Features/Metrics/Chart/HistoricChart.tsx
@@ -1,19 +1,12 @@
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { Typography, makeStyles } from "@material-ui/core";
 import useHistoricData from "../hooks/useHistoricData";
 import LineChartComponent from "./LineChart";
 import { getActiveMetrics } from "../selectors";
 import definitions from "../utils/definitions";
-
-const useStyles = makeStyles({
-  header: {
-    color: "rgba(0, 0, 0, 0.54)",
-  },
-});
+import ChartHeader from "./ChartHeader";
 
 const HistoricChart = () => {
-  const classes = useStyles();
   const [historicData, { min, max }] = useHistoricData();
   const activeMetrics = useSelector(getActiveMetrics);
 
@@ -28,9 +21,7 @@ const HistoricChart = () => {
 
   return (
     <>
-      <Typography variant="h5" className={classes.header}>
-        Historical Data
-      </Typography>
+      <ChartHeader text="Historical Data" />
       <LineChartComponent data={historicData} min={min} max={max} valueKeys={valueKeys} />
     </>
   );
